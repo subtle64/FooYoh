1b8d6ec59bbb5d77402a49195257c5c9323aa65c
Jason <143072575+subtle64@users.noreply.github.com>
2024-05-26 13:08:08 +0700

    Phase II: Added routes, enabled Supabase.

---
+++ app/(authorized)/_layout.js
@@ -0,0 +1,17 @@
+import { Redirect, Slot, router } from 'expo-router';
+import { useEffect } from 'react';
+import { supabase } from '../../lib/supabase';
+
+
+export default function Authenticate() {
+    useEffect(() => {
+        supabase.auth.getUser().then(({ data: { user } }) => {
+            if (!user) {
+                console.log("User Found")
+                router.replace("/login");
+            }
+        });
+    }, []);
+
+    return <Slot />;
+}
---
+++ app/login.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function Login() {
+    return (
+        <View>
+            <Text>Please login first.</Text>
+        </View>
+    );
+}
